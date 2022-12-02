31b7ec55f5b63146af5d83a2ad793f3d4baf7efc
tvhfrks12678 <tvhfrks12678@gmail.com>
2022-12-02 10:20:18 +0900

    feat: クイズの削除機能の実装

--- pages/quizzes/index.tsx
+++ pages/quizzes/index.tsx
@@ -9,26 +9,48 @@ type Quiz = {
 const URL = 'http://localhost:8000/api/quizzes/';
 
 const Index: FC = () => {
-  const [quizList, setQuizList] = useState<Quiz[]>([]);
+  const [quizzes, setQuizzes] = useState<Quiz[]>([]);
 
   useEffect(() => {
+    getQuizzesByUser();
+  }, []);
+
+  const getQuizzesByUser = () => {
     axios
       .get(URL)
       .then(function (response: AxiosResponse<Quiz[]>) {
         console.log(response);
-        const quizList: Quiz[] = response.data;
-        setQuizList(quizList);
+        const quizzes: Quiz[] = response.data;
+        setQuizzes(quizzes);
       })
       .catch(function (error: AxiosError) {
         console.log(error);
       });
-  }, []);
+  };
+
+  const onDeleteClicked = (id: number): void => {
+    console.log('削除');
+    axios
+      .delete(`${URL}${id}`)
+      .then((response) => {
+        console.log(response);
+        getQuizzesByUser();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <>
       <div>Quiz Index</div>
-      {quizList.map<JSX.Element>((quiz: Quiz) => {
-        return <div key={quiz.id}>{quiz.commentary}</div>;
+      {quizzes.map<JSX.Element>((quiz: Quiz) => {
+        return (
+          <div key={quiz.id}>
+            {quiz.commentary}
+            <button onClick={() => onDeleteClicked(quiz.id)}>削除</button>
+          </div>
+        );
       })}
     </>
   );
